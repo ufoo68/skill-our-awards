08ac9420382851493253b37b4cfafb3205366167
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-17 01:38:34 +0900

    update phase

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const print = require('./print')
 const functions = require('firebase-functions')
-const clova = require('@line/clova-cek-sdk-nodejs');
-const express = require('express');
+const clova = require('@line/clova-cek-sdk-nodejs')
+const express = require('express')
 
 const extensionId = functions.config().clova.extension.id
 
@@ -11,19 +11,17 @@ const clovaSkillHandler = clova.Client
 
     //起動時に喋る
     .onLaunchRequest(responseHelper => {
-        responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('今日も頑張ったあなたに賞状をあげるよ。ところで、昨日は寝ましたか？'))
+        responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('今日も頑張ったあなたに賞状をあげるよ。ところで、昨日は寝たかな？'))
     })
 
     //ユーザーからの発話が来たら反応する箇所
     .onIntentRequest(async responseHelper => {
-        const intent = responseHelper.getIntentName();
-        const slots = responseHelper.getSlots();
-
-        console.log('Intent:' + intent);
+        const intent = responseHelper.getIntentName()
+        const slots = responseHelper.getSlots()
 
         switch (intent) {
             case 'intentPrint':
-                await print('./award.pdf')
+                await print('./noaward.pdf')
                 responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('賞状を印刷したよ。')).endSession()
                 break
             case 'Clova.YesIntent':
@@ -35,24 +33,24 @@ const clovaSkillHandler = clova.Client
                 responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('賞状を印刷したよ。')).endSession()
                 break
             default:
-                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(`もう一度お願いします`));
-                break;
+                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(`もう一回行ってね。`))
+                break
         }
     })
 
     //終了時
     .onSessionEndedRequest(responseHelper => {
-        const sessionId = responseHelper.getSessionId();
+        const sessionId = responseHelper.getSessionId()
     })
-    .handle();
+    .handle()
 
-const app = new express();
+const app = new express()
 
-const clovaMiddleware = clova.Middleware({ applicationId: extensionId });
+const clovaMiddleware = clova.Middleware({ applicationId: extensionId })
 app.use( (req, res, next) => {
     req.body = JSON.stringify(req.body)
     next()
 })
-app.post('/clova', clovaMiddleware, clovaSkillHandler);
+app.post('/clova', clovaMiddleware, clovaSkillHandler)
 
-exports.clova = functions.https.onRequest(app);
+exports.clova = functions.https.onRequest(app)
