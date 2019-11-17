7ca9f967313e60a314c10559a35274f21cd2b9ac
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-17 09:30:55 +0900

    Change conversation

--- functions/index.js
+++ functions/index.js
@@ -9,45 +9,43 @@ const extensionId = functions.config().clova.extension.id
 const clovaSkillHandler = clova.Client
     .configureSkill()
 
-    //起動時に喋る
     .onLaunchRequest(responseHelper => {
         responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('今日も頑張ったあなたに賞状をあげるよ。ところで、昨日は寝たかな？'))
     })
 
-    //ユーザーからの発話が来たら反応する箇所
     .onIntentRequest(async responseHelper => {
         const intent = responseHelper.getIntentName()
-        const slots = responseHelper.getSlots()
+        const phasePrint = '賞状を印刷したよ。'
 
         switch (intent) {
             case 'intentPrint':
                 await print('./noaward.pdf')
-                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('賞状を印刷したよ。')).endSession()
+                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(phasePrint)).endSession()
                 break
             case 'Clova.YesIntent':
                 await print('./sleep.pdf')
-                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('賞状を印刷したよ。')).endSession()
+                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(phasePrint)).endSession()
                 break
             case 'Clova.NoIntent':
                 await print('./nonsleep.pdf')
-                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('賞状を印刷したよ。')).endSession()
+                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(phasePrint)).endSession()
                 break
             default:
-                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText(`もう一回行ってね。`))
+                responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('聞こえなかったよ。昨日はちゃんと寝たかな？'))
                 break
         }
     })
 
     //終了時
     .onSessionEndedRequest(responseHelper => {
-        const sessionId = responseHelper.getSessionId()
+        responseHelper.setSimpleSpeech(clova.SpeechBuilder.createSpeechText('さようなら。また呼んでね。')).endSession()
     })
     .handle()
 
 const app = new express()
 
 const clovaMiddleware = clova.Middleware({ applicationId: extensionId })
-app.use( (req, res, next) => {
+app.use( (req, _, next) => {
     req.body = JSON.stringify(req.body)
     next()
 })
